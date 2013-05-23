a3a23ecd0227050609ebaa74337e80c2af727908
cyrille (rpi) <cmeichel@free.fr>
2013-05-23 07:44:10 +0200

    led_text

---
+++ led_text/src/projet1.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <unistd.h>
+
+
+/*
+ wiringPi pin 0  is BCM_GPIO 17
+ wiringPi pin 1  is BCM_GPIO 18
+ wiringPi pin 2  is BCM_GPIO 27
+ wiringPi pin 3  is BCM_GPIO 22
+ wiringPi pin 4  is BCM_GPIO 23
+ wiringPi pin 5  is BCM_GPIO 24
+ wiringPi pin 6  is BCM_GPIO 25
+ wiringPi pin 7  is BCM_GPIO 4
+ wiringPi pin 8  is BCM_GPIO 2
+ wiringPi pin 9  is BCM_GPIO 3
+ wiringPi pin 10 is BCM_GPIO 8
+ wiringPi pin 11 is BCM_GPIO 7
+ wiringPi pin 12 is BCM_GPIO 10
+ wiringPi pin 13 is BCM_GPIO 9
+ wiringPi pin 14 is BCM_GPIO 11
+ wiringPi pin 15 is BCM_GPIO 14
+ wiringPi pin 16 is BCM_GPIO 15
+*/
+
+#define HIGH 1
+#define LOW 0
+
+/*
+ wiringPi pin 8  is BCM_GPIO 2
+ wiringPi pin 9  is BCM_GPIO 3
+ wiringPi pin 7  is BCM_GPIO 4
+ wiringPi pin 0  is BCM_GPIO 17
+ */
+#define CS0       8
+#define CS1       9
+#define CS2       7
+#define CS_ENABLE 0
+
+// HT1632C PINs
+
+#define DISPLAY_WR 4       /* BCM_GPIO 23 */
+#define DISPLAY_DATA 5     /* BCM_GPIO 24 */
+
+// HT1632C Commands
+
+#define HT1632C_READ 0b00000110
+#define HT1632C_WRITE 0b00000101
+#define HT1632C_CMD 0b00000100
+
+#define HT1632_CMD_SYSON  0x01
+#define HT1632_CMD_LEDON  0x03
+#define HT1632_CMD_SYSDIS 0x00
+#define HT1632_CMD_MASTER 0x18
+#define HT1632_CMD_SLAVE  0x10
+
+
+
+#define MATRIX struct _matrix_
+
+MATRIX {
+  unsigned char cs;
+  unsigned char addr;
+
+};
+
+
+
+void ht1632c_send_command(unsigned char command, unsigned char id);
+void ht1632c_send_bits(unsigned char bits, unsigned char firstbit);
+void chipSelect(char id);
+
+
+
+void chipSelect(char id)
+{
+  if (id<0) 
+    digitalWrite(CS_ENABLE, LOW);
+  else
+    digitalWrite(CS_ENABLE, HIGH);
+  
+  digitalWrite(CS0, id & 0x01);
+  digitalWrite(CS1, (id & 0x02)>>1);
+  digitalWrite(CS2, (id & 0x04)>>2);
+  /*printf("Chip select:\n");
+  printf("\tPin%d => %d\n", CS0, id & 0x01);
+  printf("\tPin%d => %d\n", CS1, (id & 0x02)>>1);
+  printf("\tPin%d => %d\n", CS2, (id & 0x04)>>2);*/
+}
+
+
+void initMatrix(unsigned char id)
+{
+  // Enable System oscillator and LED duty cycle generator
+  if (id==0)ht1632c_send_command(HT1632_CMD_MASTER, id);
+  else ht1632c_send_command(HT1632_CMD_SLAVE, id);
+  ht1632c_send_command(HT1632_CMD_SYSON, id);
+  ht1632c_send_command(HT1632_CMD_LEDON, id);
+}
+
+void init() {
+  unsigned char i;
+
+  // init GPIO
+  if (wiringPiSetup() == -1)
+  {
+    printf("IO is not ready\n");
+    return;
+  }
+
+  // All PINs are output
+  pinMode(CS0, OUTPUT);
+  pinMode(CS1, OUTPUT);
+  pinMode(CS2, OUTPUT);
+  pinMode(CS_ENABLE, OUTPUT);
+
+  pinMode(DISPLAY_WR, OUTPUT);
+  pinMode(DISPLAY_DATA, OUTPUT);
+  
+  for(i=0; i<4; i++)
+    initMatrix(i); 
+}
+
+
+void setAddess(unsigned int addr, MATRIX* matrix)
+{
+  matrix->addr = addr & 0x7F;
+  matrix->cs = (addr & 0x0380) >> 7; 
+}
+
+
+/*void writeData(unsigned int addr, unsigned char data, MATRIX* matrix)
+{
+  setAddress(addr, matrix);
+  
+  chipSelect(matrix->cs);
+
+  ht1632c_send_bits(HT1632C_WRITE, 0b100);
+
+  ht1632c_send_bits(matrix->addr, 0b1000000);
+
+  
+
+} */
+
+void loop(int* j) {
+  int i;
+
+  // select display
+  //chipSelect(0);
+  //digitalWrite(DISPLAY_CS, LOW);
+ 
+  // send WRITE command
+  //ht1632c_send_bits(HT1632C_WRITE, 1 << 2);
+  
+  // send start address, 00h
+  //ht1632c_send_bits(0x00, 1 << 6);
+
+  // send data
+  for(i = 0; i < 512; i++) {
+    if (i==0) {
+        chipSelect(0);
+        ht1632c_send_bits(HT1632C_WRITE, 1 << 2);
+        ht1632c_send_bits(0x00, 1 << 6);
+    }
+    if (i==256) {
+        chipSelect(1);
+        ht1632c_send_bits(HT1632C_WRITE, 1 << 2);
+        ht1632c_send_bits(0x00, 1 << 6);
+    }
+    if (i==512) {
+        chipSelect(2);
+        ht1632c_send_bits(HT1632C_WRITE, 1 << 2);
+        ht1632c_send_bits(0x00, 1 << 6);
+    }
+    if (i==768) {
+        chipSelect(3);
+        ht1632c_send_bits(HT1632C_WRITE, 1 << 2);
+        ht1632c_send_bits(0x00, 1 << 6);
+    }
+    digitalWrite(DISPLAY_WR, LOW);
+    if (i == *j) digitalWrite(DISPLAY_DATA, HIGH);
+    else digitalWrite(DISPLAY_DATA, LOW);
+    digitalWrite(DISPLAY_WR, HIGH);
+  }
+  
+  // unselect display
+  //digitalWrite(DISPLAY_CS, HIGH);
+  chipSelect(-1);
+
+  // cycle LED position to be turned on
+  if(*j < 1023) (*j)++;
+  else *j = 0;
+  
+  usleep(10000);
+}
+
+
+// ----------------------------------------
+// HT1632C functions
+// ----------------------------------------
+
+void ht1632c_send_command(unsigned char command, unsigned char id) 
+{
+  printf("Sending command %02X\n", command);
+  chipSelect(id);  
+  //digitalWrite(DISPLAY_CS, LOW);
+  ht1632c_send_bits(HT1632C_CMD, 1 << 2);
+  ht1632c_send_bits(command, 1 << 7);
+  ht1632c_send_bits(0, 1);
+  chipSelect(-1);
+  //digitalWrite(DISPLAY_CS, HIGH);
+}
+
+void ht1632c_send_bits(unsigned char bits, unsigned char firstbit) {
+  
+  while(firstbit) {
+    digitalWrite(DISPLAY_WR, LOW);
+    if (bits & firstbit) digitalWrite(DISPLAY_DATA, HIGH);
+    else digitalWrite(DISPLAY_DATA, LOW);
+    digitalWrite(DISPLAY_WR, HIGH);
+    firstbit >>= 1;
+  }
+}
+
+int main()
+{
+    int j=0;
+
+  // init GPIO
+  /*if (wiringPiSetup() == -1)
+  {
+    printf("IO is not ready\n");
+    return;
+  }
+
+  chipSelect(2);return 0;*/
+
+    printf("Setup\n");
+    init();
+    printf("Loop\n");
+	while(1) {
+		loop(&j);
+	}
+    printf("End\n");
+	return 0;
+}
